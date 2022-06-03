42ac970f06e26c15cde56b849f43a67b61d55221
Rubel Amin <bagha.vai100@gmail.com>
2022-06-03 22:02:16 +0600

    Updated

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyAl3SUhNHo0bN85gOrrMaM-TCQtFtC1FNM";
+const GOOGLE_API_KEY = "YOUR_API_KEY";
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:blue%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
